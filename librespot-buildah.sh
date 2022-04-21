72f91c0d94bb7cad99bb126dd8b699e64bec1592
benblasco <42140583+benblasco@users.noreply.github.com>
2022-04-21 15:37:24 +1000

    Reinstated librespot build commands

--- librespot-buildah.sh
+++ librespot-buildah.sh
@@ -8,10 +8,10 @@ CONTAINER=$(buildah from registry.fedoraproject.org/fedora-minimal:35)
 
 MNT=$(buildah mount $CONTAINER)
 
-#buildah run ${CONTAINER} microdnf install -y cargo make gcc alsa-lib-devel 
-#buildah run ${CONTAINER} microdnf clean all
-#buildah run ${CONTAINER} rm -rf /var/cache/dnf
-#buildah run ${CONTAINER} cargo install librespot --no-default-features --features "alsa-backend"
+buildah run ${CONTAINER} microdnf install -y cargo make gcc alsa-lib-devel 
+buildah run ${CONTAINER} microdnf clean all
+buildah run ${CONTAINER} rm -rf /var/cache/dnf
+buildah run ${CONTAINER} cargo install librespot --no-default-features --features "alsa-backend"
 
 buildah config --entrypoint "/root/.cargo/bin/librespot" ${CONTAINER} 
 #buildah config --cmd "--name=Buildah_LibreSpot_Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
