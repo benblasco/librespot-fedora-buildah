055d4ca3958e38df0f83126c8711d34ad6c363c7
benblasco <42140583+benblasco@users.noreply.github.com>
2022-04-21 14:29:18 +1000

    Grab build artefact

--- librespot-buildah.sh
+++ librespot-buildah.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+# You can enter the user namespace using the buildah unshare command.
+# If you don’t do this, the buildah mount, command will fail. 
+buildah unshare
+
 CONTAINER=$(buildah from registry.fedoraproject.org/fedora-minimal:35)
+
+MNT=$(buildah mount $CONTAINER)
 
 buildah run ${CONTAINER} microdnf install -y cargo make gcc alsa-lib-devel 
 buildah run ${CONTAINER} microdnf clean all
@@ -14,4 +20,8 @@ buildah config --cmd "--name=Buildah_LibreSpot_Speaker --bitrate=320 --zeroconf-
 buildah config --label maintainer="Benjamin Blasco" ${CONTAINER}
 buildah config --label description="Fedora-based container for running librespot, built on Rust" ${CONTAINER}
 
+cp $MNT/root/.cargo/bin/librespot /tmp
+
 buildah commit ${CONTAINER} librespot-container
+
+exit
