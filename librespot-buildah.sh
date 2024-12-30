7442f075b223ad70d7676463df9afb0b495b4143
Benjamin Blasco <bblasco@redhat.com>
2024-12-30 23:37:03 +1100

    fix: Added --locked flag to cargo install to fix dependency issue on build

--- librespot-buildah.sh
+++ librespot-buildah.sh
@@ -13,7 +13,9 @@ MNT=$(buildah mount $CONTAINER)
 buildah run ${CONTAINER} microdnf install -y cargo make gcc alsa-lib-devel 
 buildah run ${CONTAINER} microdnf clean all
 buildah run ${CONTAINER} rm -rf /var/cache/dnf
-buildah run ${CONTAINER} cargo install librespot --no-default-features --features "alsa-backend"
+# The --locked flag can be used to force Cargo to use the packaged Cargo. lock file if it is available. This may be useful for ensuring reproducible builds, to use the exact same set of dependencies that were available when the package was published.
+# Source: https://doc.rust-lang.org/cargo/commands/cargo-install.html
+buildah run ${CONTAINER} cargo install --locked librespot --no-default-features --features "alsa-backend"
 
 buildah config --entrypoint "/root/.cargo/bin/librespot" ${CONTAINER} 
 #buildah config --cmd "--name=Buildah_LibreSpot_Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
