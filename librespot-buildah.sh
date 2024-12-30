78559f0b66ef3744f85ace6ea5c43e032590a0c9
Benjamin Blasco <bblasco@redhat.com>
2024-12-30 23:47:14 +1100

    fix: Updated backend as per fix here: https://github.com/librespot-org/librespot/issues/1390

--- librespot-buildah.sh
+++ librespot-buildah.sh
@@ -15,7 +15,7 @@ buildah run ${CONTAINER} microdnf clean all
 buildah run ${CONTAINER} rm -rf /var/cache/dnf
 # The --locked flag can be used to force Cargo to use the packaged Cargo. lock file if it is available. This may be useful for ensuring reproducible builds, to use the exact same set of dependencies that were available when the package was published.
 # Source: https://doc.rust-lang.org/cargo/commands/cargo-install.html
-buildah run ${CONTAINER} cargo install --locked librespot --no-default-features --features "alsa-backend"
+buildah run ${CONTAINER} cargo install --locked librespot --no-default-features --features "alsa-backend with-libmdns"
 
 buildah config --entrypoint "/root/.cargo/bin/librespot" ${CONTAINER} 
 #buildah config --cmd "--name=Buildah_LibreSpot_Speaker --bitrate=320 --zeroconf-port=49999 --device=hdmi:CARD=PCH,DEV=0 --initial-volume=75 -v" ${CONTAINER}
